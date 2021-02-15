d84df1e0977010659e05bdcd86e8108142e0684a
Nodja <me@nodja.com>
2021-02-15 06:38:11 -0500

    fix?

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -74,9 +74,5 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    parallel: true,
-    hardSource: true,
-    cache: true
-  }
+  build: {}
 };
